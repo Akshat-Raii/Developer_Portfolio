bacf919fdaea0126419456232158e1b78ab788b4
Akshat-Raii <akshatrai7feb@gmail.com>
2025-06-30 17:36:34 +0530

    Final Commit

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_ACTIONS || false;
+import type { NextConfig } from "next";
 
-const nextConfig = {
-  output: 'export',
-  basePath: isGithubPages ? '/Developer_Portfolio' : '',
-  assetPrefix: isGithubPages ? '/Developer_Portfolio/' : '',
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
